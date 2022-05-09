508bf6fa0dee09998127782a71079d5b7c1cb5b5
Abdush <abdushs54@gmail.com>
2022-05-10 00:56:51 +0530

    changes done

--- assets/new/js/main.js
+++ assets/new/js/main.js
@@ -443,6 +443,53 @@
             ]
         });
 
+        /* ======= Hospitel ======= */
+        $('.hospital-carousel').slick('unslick')
+        // $('.hospital-carousel').slick({
+        //     dots: false,
+        //     infinite: false,
+        //     speed: 800,
+        //     slidesToShow: 3,
+        //     slidesToScroll: 2,
+        //     prevArrow: false,
+        //     nextArrow: false,
+        //     infinite: true,
+        //     pauseOnFocus: false,
+        //     accessibility: false,
+        //     pauseOnHover: true,
+        //     settings: "unslick",
+        //     responsive: [
+        //         {
+        //             breakpoint: 1024,
+        //             settings: {
+        //                 slidesToShow: 2,
+        //                 slidesToScroll: 2,
+        //             }
+        //         },
+        //         {
+        //             breakpoint: 992,
+        //             settings: {
+        //                 slidesToShow: 2,
+        //                 slidesToScroll: 2,
+        //             }
+        //         },
+        //         {
+        //             breakpoint: 767,
+        //             settings: {
+        //                 slidesToShow: 1,
+        //                 slidesToScroll: 1,
+        //             }
+        //         },
+        //         {
+        //             breakpoint: 580,
+        //             settings: {
+        //                 slidesToShow: 1,
+        //                 slidesToScroll: 1,
+        //             }
+        //         }
+        //     ]
+        // });
+
         /* ======= Odometer ======= */
         $('.odometer').waypoint(
             function () {
--- hospital.php
+++ hospital.php
@@ -1,26 +1,259 @@
-<?php include 'header.php'?>
-<div class="container-fulid">
-<div class="page-head_agile_info_w3l">
-		<div class="container">
-			<h3>Hospital</h3>
-			<!--/w3_short-->
-				 <div class="services-breadcrumb">
-						<div class="agile_inner_breadcrumb">
+<?php 
+include 'header.php';
+include 'connect.php';
+?>
+<section class="page-header padding">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="page-content text-center">
+            <h2>Hospitals</h2>
+            <div class="page-item">
+                <a href="/"><i class="ti-home"></i>Home </a>
+                <p>Hospitals</p>
+            </div>
+        </div>
+    </div>
+</section>
 
-						   <ul class="w3_short">
-								<li><a href="index.html">Home</a><i style="color:black">|</i></li>
-								<li style='color:black'>Hospital</li>
-							</ul>
-						 </div>
-				</div>
-	   <!--//w3_short-->
-	</div>
+<section class="event-section-2 bd-bottom padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 padding-15">
+                    <div class="event-item">
+                        <div class="event-thumb">
+                            <img src="assets/new/img/event-1-450x420.jpg" alt="event">
+                            <div class="date bg-red">
+                                <h3>28<span>Jan</span></h3>
+                            </div>
+                        </div>
+                        <div class="event-content text-center">
+                            <a href="event-details.html">
+                                <h3>Fight For Right Cause</h3>
+                            </a>
+                            <ul class="event-list">
+                                <li><i class="fas fa-clock"></i>10:00 AM</li>
+                                <li><i class="fas fa-map-marker-alt"></i>New Work</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 padding-15">
+                    <div class="event-item">
+                        <div class="event-thumb">
+                            <img src="assets/new/img/event-2-450x420.jpg" alt="event">
+                            <div class="date bg-yellow">
+                                <h3>28<span>sep</span></h3>
+                            </div>
+                        </div>
+                        <div class="event-content text-center">
+                            <a href="event-details.html">
+                                <h3>Education For Children</h3>
+                            </a>
+                            <ul class="event-list">
+                                <li><i class="fas fa-clock"></i>09:00 AM</li>
+                                <li><i class="fas fa-map-marker-alt"></i>New Work</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 padding-15">
+                    <div class="event-item">
+                        <div class="event-thumb">
+                            <img src="assets/img/event-3-450x420.jpg" alt="event">
+                            <div class="date bg-blue">
+                                <h3>28<span>Feb</span></h3>
+                            </div>
+                        </div>
+                        <div class="event-content text-center">
+                            <a href="#">
+                                <h3>Help For Education</h3>
+                            </a>
+                            <ul class="event-list">
+                                <li><i class="fas fa-clock"></i>11:00 AM</li>
+                                <li><i class="fas fa-map-marker-alt"></i>New Work</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 padding-15">
+                    <div class="event-item">
+                        <div class="event-thumb">
+                            <img src="assets/img/event-4-450x420.jpg" alt="event">
+                            <div class="date bg-purple">
+                                <h3>28<span>Jun</span></h3>
+                            </div>
+                        </div>
+                        <div class="event-content text-center">
+                            <a href="event-details.html">
+                                <h3>Water For Children</h3>
+                            </a>
+                            <ul class="event-list">
+                                <li><i class="fas fa-clock"></i>12:00 AM</li>
+                                <li><i class="fas fa-map-marker-alt"></i>New Work</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 padding-15">
+                    <div class="event-item">
+                        <div class="event-thumb">
+                            <img src="assets/img/event-5-450x420.jpg" alt="event">
+                            <div class="date bg-green">
+                                <h3>28<span>Aug</span></h3>
+                            </div>
+                        </div>
+                        <div class="event-content text-center">
+                            <a href="event-details.html">
+                                <h3>Protect The Eco System</h3>
+                            </a>
+                            <ul class="event-list">
+                                <li><i class="fas fa-clock"></i>01:00 AM</li>
+                                <li><i class="fas fa-map-marker-alt"></i>New Work</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 padding-15">
+                    <div class="event-item">
+                        <div class="event-thumb">
+                            <img src="assets/img/event-6-450x420.jpg" alt="event">
+                            <div class="date bg-pink">
+                                <h3>28<span>Mar</span></h3>
+                            </div>
+                        </div>
+                        <div class="event-content text-center">
+                            <a href="event-details.html">
+                                <h3>Help For Rohingya People</h3>
+                            </a>
+                            <ul class="event-list">
+                                <li><i class="fas fa-clock"></i>08:00 AM</li>
+                                <li><i class="fas fa-map-marker-alt"></i>New Work</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<section class="event-section-2 bd-bottom padding">
+    <div class="container">
+        <div class="row">
+            <?php
+				
+				$sql = "SELECT * FROM tie_up where type='Hospital'";
+				$result = $conn->query($sql);
+				
+				if ($result->num_rows > 0) {
+					// output data of each row
+					while($row = $result->fetch_assoc()) { ?>
+            <div class="col-lg-4 col-md-6 padding-15">
+                <div class="event-item">
+                    <div class="event-thumb">
+                        <img src="assets/new/img/hospital.png" alt="event">
+                        <div class="date bg-red">
+                            <h3><?php echo $row['percent']." OFF";?></h3>
+                        </div>
+                    </div>
+                    <div class="event-content text-center">
+                        <a href="event-details.html">
+                            <h3><?php echo $row['hc_name'];?></h3>
+                        </a>
+                        <ul class="event-list">
+
+                            <li><i class="fas fa-map-marker-alt"></i><?php echo $row['office_address'];?></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <?php
+                }
+                } else {
+                echo "0 results";
+                }
+
+
+                ?>
+
+        </div>
+    </div>
+</section>
+<!-- 
+    <section class="event-section bd-bottom padding">
+        <div class="container">
+            <div class="section-heading mb-40 text-center">
+                <span class="sub-heading">Hospitals</span>
+                <h2>Hospitals</h2>
+                <p>Charitable giving as a religious act or duty is referred to as alms. The name <br>stems from the most
+                    obvious expression of the virtue of charity.</p>
+            </div><!-- /.section-heading -->
+<div class="slider hospital-carousel nav-style carousel-dots">
+    <?php
+				
+				$sql = "SELECT * FROM tie_up where type='Hospital'";
+				$result = $conn->query($sql);
+				
+				if ($result->num_rows > 0) {
+					// output data of each row
+					while($row = $result->fetch_assoc()) { ?>
+
+
+
+    <div class="event-item">
+
+        <div class="event-thumb">
+            <img src="assets/new/img/hospital.png" alt="event">
+            <div class="date bg-red">
+                <h3><?php echo $row['percent']." OFF";?></h3>
+            </div>
+        </div>
+        <div class="event-content text-center">
+            <a href="event-details.html">
+                <h3><?php echo $row['hc_name'];?></h3>
+            </a>
+            <ul class="event-list">
+
+                <li><i class="fas fa-map-marker-alt"></i><?php echo $row['office_address'];?></li>
+            </ul>
+        </div>
+    </div>
+
+    <!-- echo "<div class='col-sm-4' style='margin-top:20px;'>
+                    <div style='box-shadow:0px 10px 10px 0px;width:96%;border-radius:6px;height:300px'>
+                        <div style='width:98%;margin-top:20px;margin-bottom:20px;margin-left:7px;'>
+                            <h4><br><b>".$row['hc_name']."</b></h4><br><span><i
+                                    class='fa fa-map'></i>&nbsp;&nbsp;".$row['office_address']."</span><br><br><span><b>Discount
+                                    :</b>&nbsp;&nbsp;".$row['percent']."</span>
+                            <hr><a href='hospital_lab_medical_read.php?id=".$row[' id']."' class='btn btn-success'>Read
+                                More</a>
+                            <hr>
+                        </div>
+                    </div>
+                </div>"; -->
+    <?php
+                }
+                } else {
+                echo "0 results";
+                }
+
+
+                ?>
+
 </div>
+</div>
+</section> -->
+
+<!-- ./ event-section -->
+
+
 <div class="container">
-	<div class="row" style="margin-top:50px">
-		<center><h3><u><b>Hospital</b></u></h3></center>
-		<br>
-		<?php
+    <div class="row" style="margin-top:50px">
+        <center>
+            <h3><u><b>Hospital</b></u></h3>
+        </center>
+        <br>
+        <?php
             include 'connect.php';
             
             $sql = "SELECT * FROM tie_up where type='Hospital'";
@@ -37,11 +270,10 @@
             
             $conn->close();
         ?>
-	
-		</div>	<br></br>
-	</div>
+
+    </div> <br></br>
+</div>
 </div>
 
 
 <?php include 'footer.php'?>
-  
